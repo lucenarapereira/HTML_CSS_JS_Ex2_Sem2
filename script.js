ac2b9aa12852fa516c71240434b935eca7070b51
lucenarapereira <93477361+lucenarapereira@users.noreply.github.com>
2021-11-01 09:57:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+Utilizando Javascript, emita um alerta para o usuário com uma informação importante.
+ 
+
+Dica: Lembre-se da estrutura “scripts” criada no exercício anterior.
+https://developer.mozilla.org/pt-BR/docs/Web/API/Window/alert
+
+*/
+
+function Alert(){
+  window.alert(`Parabéns!`)
+}
+
+Alert();
