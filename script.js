2c2095073bf9bd37add0c2114c65e56a407114d2
lucenarapereira <93477361+lucenarapereira@users.noreply.github.com>
2021-11-02 14:05:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/*
-Utilizando Javascript, emita um alerta para o usuário com uma informação importante.
- 
-
-Dica: Lembre-se da estrutura “scripts” criada no exercício anterior.
-https://developer.mozilla.org/pt-BR/docs/Web/API/Window/alert
-
-*/
-
 function Alert(){
   window.alert(`Parabéns!`)
 }
